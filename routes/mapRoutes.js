e484402bec9175dbb24ca3a1cd160579d4825ab8
Chris Coutts <chris.dcoutts@gmail.com>
2024-01-15 10:21:20 +0000

    Set up POST request

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -11,11 +11,53 @@ router.get("/", (req, res) => {
   const pubs = JSON.parse(pubsFile);
 
   const pubList = pubs.map((pub) => ({
-    PubName: pub.PubName,
-    Address: pub.Address,
+    id: pub.id,
+    pub: pub.pub,
+    address: pub.address,
   }));
 
   res.json(pubList);
+});
+
+// POST /maps
+
+router.post("/", (req, res) => {
+  // const geoForwardUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+  // // const accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+
+  // const geocodeAddress = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `${geoForwardUrl}${encodeURIComponent(
+  //         address
+  //       )}.json?access_token=${accessToken}`
+  //     );
+
+  //     const coordinates = response.data.features[0].geometry.coordinates;
+  //   } catch (error) {
+  //     console.error(`Error geocoding ${address}: ${error.message}`);
+  //   }
+  // };
+
+  // geocodeAddress();
+
+  const newPub = {
+    id: crypto.randomUUID(),
+    pub: req.body.pub,
+    address: req.body.address,
+  };
+
+  if (!newPub.id) {
+    return res.status(400).json({ error: "Please provide a pub id" });
+  }
+
+  const pubData = JSON.parse(fs.readFileSync("./data/pubs.json"));
+
+  pubData.push(newPub);
+
+  fs.writeFileSync("./data/pub.json", JSON.stringify(pubData));
+
+  res.status(201).json(pubData);
 });
 
 module.exports = router;
