9fcaad92dbe6dd4f900ab65b241ae128a01d770e
Chris Coutts <chris.dcoutts@gmail.com>
2024-02-01 17:20:10 +0000

    Add JWT token

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-require("dotenv").config();
 
 module.exports = {
   client: "mysql2",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,84 +1,88 @@
-// const express = require("express");
-// const router = express.Router();
-// const fs = require("fs");
-// const cors = require("cors");
-// const crypto = require("crypto");
-// const axios = require("axios");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const cors = require("cors");
+const crypto = require("crypto");
+const axios = require("axios");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const knex = require("knex")(require("../knexfile"));
 
-// router.post("/register", async(req, res) => {
-//     const {user_name, address, email, password} = req.body;
+router.post("/signup", async (req, res) => {
+  const { user_name, address, email, password } = req.body;
 
-//     if (!user_name || address || email || password){
-//         return res.status(400).send("Please enter the required fields.");
-//     }
+  if (!user_name || !address || !email || !password) {
+    return res.status(400).send("Please enter the required fields.");
+  }
 
-//     const hashedPassword = bcrypt.hashSync(password, 6);
+  const hashedPassword = bcrypt.hashSync(password.toString(), 6);
 
-//     const newUser = {
-//         user_name,
-//         address,
-//         email,
-//         role: "standard user"
-//         password: hashedPassword,
-//     };
+  const newUser = {
+    user_name,
+    address,
+    email,
+    role: "user",
+    password: hashedPassword,
+  };
 
-//     try {
-//         await knew("users").insert(newUser);
-//         return res.status(201).json(newUser);
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(400).json({ message: "Failed registration" });
-//     }
-// });
+  try {
+    await knex("user").insert(newUser);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Failed registration" });
+  }
+});
 
-// router.post("/login", async (req, res) => {
-//     const {user_name, password} = req.body;
+router.post("/login", async (req, res) => {
+  const { user_name, password } = req.body;
 
-//     if (!user_name || !password) {
-//         return res.status(400).send("Please enter the required fileds");
-//     }
+  if (!user_name || !password) {
+    return res.status(400).send("Please enter the required fields");
+  }
 
-//     try {
-//         const user = await knex("users").where({user_name: user_name}).first();
+  try {
+    const user = await knex("user").where({ user_name: user_name }).first();
 
-//         const passwordCorrect = bcrypt.compareSync(password, user.password);
+    const passwordCorrect = bcrypt.compareSync(password.toString(), user.password);
 
-//         if(!passwordCorrect){
-//             return res.status(400).send("Invalid passwrod");
-//         }
+    if (!passwordCorrect) {
+      return res.status(400).send("Invalid password");
+    }
 
-//         const token = jwt.sign({ id: user.id, email: user.id }, process.env.JWT_SECRET,
-//             {
-//                 expiresIn:"24h",
-//             });
+    const token = jwt.sign(
+      { id: user.id, password: user.password },
+      `${process.env.JWT_SECRET}`,
+      {
+        expiresIn: "24h",
+      }
+    );
 
-//             return res.json({ token });
-//     } catch (error){
-//         console.log(error);
-//         return res.status(400).json({ message: "Failed login"});
-//     }
-// });
+    return res.json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Failed login" });
+  }
+});
 
-// router.get("profile", async (req, res) => {
-//     if (!req.headers.authorization) {
-//         return res.status(401).send("Please login");
-//     }
+router.get("/profile", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Please login");
+  }
 
-//     const authHeader = req.headers.authorization;
-//     const authToken = authHeader.split(" ")[1];
+  const authHeader = req.headers.authorization;
+  const authToken = authHeader.split(" ")[1];
 
-//     try {
-//         const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
+  try {
+    const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
 
-//     const user = await knex("users").where({ id: decoded.id }).first();
+    const user = await knex("user").where({ id: decoded.id }).first();
 
-//     res.json(user);
-// } catch(error){
-//     console.log(error);
-//     res.status(400).send("Invalid auth token");
-// }
-// });
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Invalid auth token");
+  }
+});
 
-// module.exports = router;
+module.exports = router;
